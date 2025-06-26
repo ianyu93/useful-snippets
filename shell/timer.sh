9ed379084987e2d71b584888e0b126ef8a63f6a2
Ian Yu <ian.yu@arc.com.co>
2025-06-26 13:21:28 -0400

    feat(sh): add timer

---
+++ shell/timer.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+format_time() {
+    local total_seconds=$1
+    printf "%02d:%02d:%02d" $((total_seconds/3600)) $(((total_seconds%3600)/60)) $((total_seconds%60))
+}
+
+# Colors
+if command -v tput >/dev/null 2>&1; then
+    BOLD=$(tput bold)
+    GREEN=$(tput setaf 2)
+    YELLOW=$(tput setaf 3)
+    BLUE=$(tput setaf 4)
+    RESET=$(tput sgr0)
+else
+    BOLD="" GREEN="" YELLOW="" BLUE="" RESET=""
+fi
+
+echo "${GREEN}${BOLD}Timer started...${RESET} (Press Enter to stop)"
+
+start_time=$(date +%s)
+
+while true; do
+    elapsed=$(($(date +%s) - start_time))
+    printf "\r${YELLOW}${BOLD}Elapsed: %s${RESET}" "$(format_time $elapsed)"
+    
+    # Check if Enter was pressed (with 1 second timeout)
+    if read -t -r 1; then
+        break
+    fi
+done
+
+echo
+echo "${BLUE}${BOLD}Timer stopped at: $(format_time "$elapsed")${RESET}"
